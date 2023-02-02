eac6bf2a5500ab8a64b7d6bd99e239488e538dd5
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-02-02 13:04:52 +0900

    ADD - realtime clock

--- client/src/Routes/Home.tsx
+++ client/src/Routes/Home.tsx
@@ -28,14 +28,26 @@ const Img = styled.img`
 
 function Home() {
   const [bgImage, setbgImage] = useState("");
+  const [date, setDate] = useState("");
+  function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    setDate(`${hours}:${minutes}:${seconds}`);
+  }
   useEffect(() => {
     setbgImage(bgImages[Math.floor(Math.random() * bgImages.length)]);
-    console.log(bgImage);
+    const clockId = setInterval(getClock, 1000);
+    return function cleanup() {
+      clearInterval(clockId);
+    };
   }, [bgImage]);
   return (
     <Wrapper bgImage={bgImage}>
       <Main>
         <Img src={img}></Img>
+        <span>{date}</span>
       </Main>
     </Wrapper>
   );
