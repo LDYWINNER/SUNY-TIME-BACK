18f8d4c59f660fa8669b89b1150dbfabf8df8e73
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-02-04 21:13:28 +0900

    DELETE - normalize.css

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "normalize.css";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
