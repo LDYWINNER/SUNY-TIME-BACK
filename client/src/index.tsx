6bff798bdc40a1ddedce0087f40b3e17f5bb7727
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-02-04 21:10:34 +0900

    ADD - normalize.css

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "normalize.css";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
