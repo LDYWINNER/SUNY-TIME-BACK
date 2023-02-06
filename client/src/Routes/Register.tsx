003c502b561c2cbf39155a3b978ccd3be0f5c4ea
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-02-06 20:39:47 +0900

    ADD - register.tsx logic and style working + FIX - framer motion error fixed + global style template added: index.css

--- client/src/Routes/Register.tsx
+++ client/src/Routes/Register.tsx
@@ -1,10 +1,32 @@
 import { Header } from "../Components";
+import logo from "../assets/images/navbar_logo.svg";
+import Wrapper from "../assets/wrappers/Register";
+import { useState } from "react";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 function Register() {
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
   return (
     <>
       <Header />
-      <h1>Register</h1>
+      <Wrapper className="full-page">
+        <form className="form"></form>
+      </Wrapper>
     </>
   );
 }
