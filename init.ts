43e4e4c6909e5c2283559c2e06456c122e88e3e3
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-02-07 17:09:49 +0900

    ADD - DB connected

--- init.ts
+++ init.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import connectDB from "./db/connect";
 import app from "./server";
 
 const port = process.env.PORT || 4001;
@@ -6,4 +7,13 @@ const port = process.env.PORT || 4001;
 const handleListening = () =>
   console.log(`Server Listening on port http://localhost:${port}`);
 
-app.listen(port, handleListening);
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, handleListening);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
