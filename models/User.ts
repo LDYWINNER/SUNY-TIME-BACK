de248953dfc797e8b3040a4be493b7a3b197182e
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-02-10 01:53:27 +0900

    ADD - User model

---
+++ models/User.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide name"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 8,
+  },
+  school: {
+    type: String,
+    required: [true, "Please provide your school info"],
+  },
+  major: {
+    type: String,
+    required: [true, "Please provide your major"],
+  },
+});
+
+export default mongoose.model("User", UserSchema);
