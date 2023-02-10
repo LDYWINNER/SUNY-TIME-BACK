9810627f6170c3b5ca2c1361fdaae205a53d2589
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-02-10 23:47:14 +0900

    ADD - handling unique field error

--- middleware/error-handler.ts
+++ middleware/error-handler.ts
@@ -18,12 +18,22 @@ const errorHandlerMiddleware = (
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
     msg: "Something went wrong, try again later",
   };
+  //missing field error
   if (err.name === "ValidationError") {
     defaultError.statusCode = StatusCodes.BAD_REQUEST;
     // defaultError.msg = err.message;
     defaultError.msg = Object.values(err.errors)
       .map((item: IItem) => item.message)
       .join(", ");
+  }
+  //unique field error
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(
+      err.keyValue
+    )} field has to be unique, your ${Object.keys(
+      err.keyValue
+    )} already exists`;
   }
   // res.status(defaultError.statusCode).json({ msg: err });
   res.status(defaultError.statusCode).json({ msg: defaultError.msg });
