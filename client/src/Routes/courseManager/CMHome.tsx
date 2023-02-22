d2ac853c2d34ad26af55dff95337cee29d2ba131
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-02-22 22:24:24 +0900

    ADD - react-timetable-events

--- client/src/Routes/courseManager/CMHome.tsx
+++ client/src/Routes/courseManager/CMHome.tsx
@@ -1,7 +1,53 @@
+import Timetable from "react-timetable-events";
+
+interface Event {
+  id: number | string;
+  name: string;
+  startTime: Date;
+  endTime: Date;
+  type?: string;
+  [key: string]: unknown;
+}
+
+interface Events {
+  [day: string]: Event[];
+}
+
+const events: Events = {
+  monday: [
+    {
+      id: 1,
+      name: "Custom Event 1",
+      type: "custom",
+      startTime: new Date("2018-02-23T11:30:00"),
+      endTime: new Date("2018-02-23T13:30:00"),
+    },
+  ],
+  tuesday: [
+    {
+      id: 2,
+      name: "Custom Event 2",
+      type: "custom",
+      startTime: new Date("2018-02-22T12:30:00"),
+      endTime: new Date("2018-02-22T14:30:00"),
+    },
+    {
+      id: 3,
+      name: "Custom Event 3",
+      type: "custom",
+      startTime: new Date("2018-02-22T16:30:00"),
+      endTime: new Date("2018-02-22T18:45:00"),
+    },
+  ],
+  wednesday: [],
+  thursday: [],
+  friday: [],
+};
+
 function CMHome() {
   return (
     <>
-      <h1>Course Manager Home</h1>
+      <Timetable events={events} style={{ height: "500px" }} />
     </>
   );
 }
