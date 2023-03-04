e9a3048462eb8f02727cee03faa56c2b8916f3e1
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-03-04 10:05:05 +0900

    ADD - createBulletinPost controller

--- controllers/bulletinPostController.ts
+++ controllers/bulletinPostController.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from "express";
+import BulletinPost from "../models/BulletinPost";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, UnAuthenticatedError } from "../errors";
 
 const createBulletinPost = async (req: Request, res: Response) => {
-  res.send("createBulletinPost");
+  const { title, content, board } = req.body;
+
+  if (!title || !content || !board) {
+    throw new BadRequestError("Please provide all values");
+  }
+
+  req.body.createdBy = req.user?.userId;
+  const post = await BulletinPost.create(req.body);
+  res.status(StatusCodes.CREATED).json({ post });
 };
 
 const getAllBulletinPosts = async (req: Request, res: Response) => {
--- dist/controllers/bulletinPostController.js
+++ dist/controllers/bulletinPostController.js
@@ -8,10 +8,23 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.showStats = exports.updateBulletinPost = exports.getAllBulletinPosts = exports.deleteBulletinPost = exports.createBulletinPost = void 0;
+const BulletinPost_1 = __importDefault(require("../models/BulletinPost"));
+const http_status_codes_1 = require("http-status-codes");
+const errors_1 = require("../errors");
 const createBulletinPost = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    res.send("createBulletinPost");
+    var _a;
+    const { title, content, board } = req.body;
+    if (!title || !content || !board) {
+        throw new errors_1.BadRequestError("Please provide all values");
+    }
+    req.body.createdBy = (_a = req.user) === null || _a === void 0 ? void 0 : _a.userId;
+    const post = yield BulletinPost_1.default.create(req.body);
+    res.status(http_status_codes_1.StatusCodes.CREATED).json({ post });
 });
 exports.createBulletinPost = createBulletinPost;
 const getAllBulletinPosts = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
