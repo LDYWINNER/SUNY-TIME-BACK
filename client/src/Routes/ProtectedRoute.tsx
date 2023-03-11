794f22922f89fce5ee8b91ddf6b1269db6d05ece
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-03-11 16:04:24 +0900

    UPDATE - ProtectedRoute from localStorage & to /register

--- client/src/Routes/ProtectedRoute.tsx
+++ client/src/Routes/ProtectedRoute.tsx
@@ -1,11 +1,15 @@
-import { useRecoilValue } from "recoil";
-import { globalCurrentState } from "../atoms";
+// import { useRecoilValue } from "recoil";
+// import { globalCurrentState } from "../atoms";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }: any) => {
-  const { user } = useRecoilValue(globalCurrentState);
+  // const { user } = useRecoilValue(globalCurrentState);
+  // if (!user) {
+  //   return <Navigate to="/" />;
+  // }
+  const user = localStorage.getItem("user");
   if (!user) {
-    return <Navigate to="/" />;
+    return <Navigate to="/register" />;
   }
   return children;
 };
