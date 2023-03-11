13dd9ab34d8759c1dc9a5cfe73887ad02070a926
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-03-11 17:32:47 +0900

    UPDATE - using state with useLocation

--- client/src/Routes/SinglePost.tsx
+++ client/src/Routes/SinglePost.tsx
@@ -1,7 +1,37 @@
+import { useLocation } from "react-router-dom";
+
+interface IPostComment {
+  content: string;
+  likes: number;
+  dislikes: number;
+  createdBy: string;
+  updatedAt: string;
+}
+
+interface RouteState {
+  comments: [IPostComment];
+  anonymity: Boolean;
+  board: string;
+  content: string;
+  createdAt: string;
+  createdBy: string;
+  dislikes: number;
+  existingBoard: string;
+  likes: number;
+  newBoard: string;
+  title: string;
+  updatedAt: string;
+  __v: number;
+  _id: string;
+}
+
 function SinglePost() {
+  const location = useLocation();
+  const state = location.state as RouteState;
+
   return (
     <div>
-      <h1>hello</h1>
+      <h1>{state.title}</h1>
     </div>
   );
 }
