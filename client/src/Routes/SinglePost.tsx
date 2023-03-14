4ab870d9c59cfdb568a337e3aacdf62833b616f5
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-03-14 22:51:44 +0900

    ADD - delete post frontend

--- client/src/Routes/SinglePost.tsx
+++ client/src/Routes/SinglePost.tsx
@@ -1,13 +1,16 @@
 import moment from "moment";
 import { useLocation } from "react-router-dom";
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { authFetch } from "../api";
 import {
   Wrapper,
   Container,
   TitleRow,
   Title,
 } from "../assets/wrappers/SinglePost";
-import { bulletinBgImageState } from "../atoms";
+import { bulletinBgImageState, globalCurrentState } from "../atoms";
+import { removeUserFromLocalStorage } from "../utils";
+import { useNavigate } from "react-router-dom";
 
 interface IPostComment {
   content: string;
@@ -35,9 +38,35 @@ interface RouteState {
 }
 
 function SinglePost() {
+  const navigate = useNavigate();
   const bgImage = useRecoilValue(bulletinBgImageState);
   const location = useLocation();
   const state = location.state as RouteState;
+  const [globalState, setGlobalCurrentState] =
+    useRecoilState(globalCurrentState);
+
+  const logoutUser = () => {
+    setGlobalCurrentState((currentState) => {
+      return {
+        ...currentState,
+        user: null,
+        token: null,
+      };
+    });
+    removeUserFromLocalStorage();
+    window.location.reload();
+  };
+
+  const deletePost = async (id: string) => {
+    try {
+      await authFetch.delete(`/bulletin/${id}`);
+      navigate("/bulletin");
+    } catch (error) {
+      console.log(error);
+      // log user out
+      logoutUser();
+    }
+  };
 
   return (
     <Wrapper bgImage={bgImage}>
@@ -56,7 +85,7 @@ function SinglePost() {
         <button
           type="button"
           className="btn delete-btn"
-          onClick={() => "delete"}
+          onClick={() => deletePost(state._id)}
         >
           DELETE
         </button>
