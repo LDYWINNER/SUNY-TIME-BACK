8b15cef7842ac289bc8ff07f334f05d3dafee675
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-04-18 13:54:23 +0900

    UPDATE - footer temp design

--- client/src/Components/Footer.tsx
+++ client/src/Components/Footer.tsx
@@ -1,16 +1,50 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import logo from "../assets/images/navbar_logo.svg";
 
 const Wrapper = styled.div`
   background-color: black;
   color: white;
   width: 100%;
   height: 200px;
+  padding-top: 20px;
+  display: flex;
+  align-items: flex-start;
+  padding-left: 10px;
+`;
+
+const Logo = styled.img`
+  display: block;
+  width: 15%;
+`;
+
+const Content = styled.div`
+  color: white;
+  display: flex;
+  flex-direction: column;
+  font-weight: 500;
+  margin-left: 80px;
+  margin-top: 10px;
+  a {
+    margin-bottom: 10px;
+  }
 `;
 
 const Footer = () => {
   return (
     <Wrapper>
-      <h4>hello</h4>
+      <Logo src={logo} alt="sunytime" className="logo" />
+      <Content>
+        <Link to="">
+          <span>FAQ</span>
+        </Link>
+        <Link to="">
+          <span>Contact</span>
+        </Link>
+        <Link to="">
+          <span>Feedback</span>
+        </Link>
+      </Content>
     </Wrapper>
   );
 };
