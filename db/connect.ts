69bb1235493e3c79b98afd9fc0ce836c44c321df
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-04-22 00:24:25 +0900

    DEPLOY - uri problem again

--- db/connect.ts
+++ db/connect.ts
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 mongoose.set("strictQuery", true);
 
 const connectDB = (url: string) => {
-  return mongoose.connect(url, { useNewUrlParser: true });
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
 export default connectDB;
