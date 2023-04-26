a29ff6c84062a5e4eb3879d38d455e3adad0e83d
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-04-26 12:35:04 +0900

    UPDATE - search filter dark light mode

--- client/src/assets/wrappers/CourseSearch.ts
+++ client/src/assets/wrappers/CourseSearch.ts
@@ -10,23 +10,23 @@ export const Wrapper = styled.div`
     cursor: pointer;
     height: 30px;
     padding: 5px 15px;
-    border: 1px solid #333;
+    border: 1px solid ${(props) => props.theme.textColor.lighter};
     border-radius: 15px;
     margin-right: 5px;
     line-height: 24px;
     text-align: center;
     font-weight: bold;
     font-size: 13px;
-    background-color: #fff;
-    color: #333;
+    background-color: ${(props) => props.theme.bgColor.lighter};
+    color: ${(props) => props.theme.textColor.lighter};
     display: flex;
     align-items: center;
     justify-content: center;
   }
 
   input[type="radio"]:checked + label {
-    background-color: #333;
-    color: #fff;
+    background-color: ${(props) => props.theme.textColor.lighter};
+    color: ${(props) => props.theme.bgColor.lighter};
   }
 `;
 
