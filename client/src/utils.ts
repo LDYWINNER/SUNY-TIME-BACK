da2441807a3cbec3397e73881a363ffda22658d9
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-04-26 21:24:18 +0900

    ADD - instructor array

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -54,3 +54,80 @@ export const removeUserFromLocalStorage = () => {
   localStorage.removeItem("user");
   localStorage.removeItem("token");
 };
+
+export const amsInstructors = [
+  "Kazem Mahdavi",
+  "Young-seon Lee",
+  "Ky Tran",
+  "Tan Cao",
+  "Suil O",
+  "CHANGSOON PARK",
+  "Hongshik Ahn",
+  "Hyunwook Koh",
+];
+
+export const accInstructors = ["Jeehong Kim", "Sunghee Kim", "Soo Young Kwon"];
+
+export const busInstructors = [
+  "Jaeyeong Lee",
+  "Chungseung Lee",
+  "Noelle (Myunghun) Chung",
+  "Chih-mao Hsieh",
+  "Joung Hwa Choi",
+  "Sung Sup Choi",
+  "Dae Ryun Chang",
+  "KYUNG KU KIM",
+  "In Ki Joo",
+  "Young-Won Ha",
+
+  "Soo Young Kwon",
+  "Jeehong Kim",
+];
+
+export const cseInstructors = [
+  "Francois Rameau",
+  "Antonino Mione",
+  "Byungkon Kang",
+  "Zhoulai Fu",
+  "Amos Omondi",
+  "Jihoon Ryoo",
+  "Arthur Lee",
+  "Seung Soo Park",
+  "Chi Him Wong",
+];
+
+export const eseInstructors = ["Sangjin Hong", "Cornelius Bradter"];
+
+export const estInstructors = [
+  "Maurice Bess",
+  "Joseph Cabuay",
+  "Anthony Pennings",
+  "James Larson",
+  "Neal Dreamson",
+  "Cornelius Bradter",
+  "Su Hyeong Kim",
+  "Suzana Brown",
+  "Gayoung Park",
+  "Jang Lee",
+  "Sira Maliphol",
+  "Jin-sang Lee",
+  "Mark Whitaker",
+  "Young-Geun Lee",
+  "Johng-Ihl Lee",
+  "Gerald Stokes",
+];
+
+export const empInstructors = ["Johng-Ihl Lee", "Sung Jin Kim"];
+
+export const mecInstructors = [
+  "Cornelius Bradter",
+  "Jong Park",
+  "Hamid Hefazi",
+  "Gun Woong Bahng",
+  "Changwoon Han",
+  "JONGSEONG CHOI",
+  "Y Pak",
+  "Seung Bok Choi",
+  "Min Yang Yang",
+  "Joon Sik Lee",
+];
